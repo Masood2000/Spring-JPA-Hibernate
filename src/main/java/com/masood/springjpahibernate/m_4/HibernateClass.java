dbbd9cdd367ef9f9b77d3c66ffdf8ee30d13f1af
Masood2000 <masudurrehman2000@gmail.com>
2026-02-27 22:05:40 +0100

    module-start: 4

---
+++ src/main/java/com/masood/springjpahibernate/m_4/HibernateClass.java
@@ -0,0 +1,79 @@
+package com.masood.springjpahibernate.m_4;
+
+import com.masood.springjpahibernate.persistence.CustomPersistenceUnitInfo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import java.util.HashMap;
+
+
+// em.persist() -> Adding an entity in context
+// em.find()    -> Find by PK. Get from DB and add it to the context if not already.
+// em.remove()  -> Marking entity for removal.
+// em.merge()   -> Merges an entity from outside the context to the context.
+// em.refresh() -> Mirror the context from the database.
+// em.detach()  -> Taking entity out of the context.
+// em.getReference() -> query is generated when you do something with the object, like the lazy initialization in kotlin
+
+public class HibernateClass {
+
+
+
+    public  static void main(String[] args) {
+
+        String puName = "oraclePU";
+        String password = "";
+
+        HashMap<String,String> props = new HashMap<>();
+        props.put("hibernate.show_sql","true");
+        props.put("hibernate.hbm2ddl.auto","update"); //(create tables automatically) //always use with examples and tutorials. none -> production, create -> drop then create, update -> update tables only.
+
+        //EntityManagerFactory emf = Persistence.createEntityManagerFactory("oraclePU");
+        EntityManagerFactory emf = new HibernatePersistenceProvider()
+                .createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(puName,password), props);
+
+
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+
+
+
+            em.getTransaction().commit();
+
+            //refresh
+            /*
+             Refresh
+             var e1 = em.find(Employee.class,1);
+
+             e1.setName("Masood ur Rehman22");
+
+             em.refresh(e1);
+             */
+            //persist, find
+            /*
+                        Employee ee = new Employee();
+                        ee.setId(1L);
+                        ee.setAddress("Ramonville");
+                        ee.setName("Masood");
+
+                        em.persist(ee);
+
+                        Employee ee2 = em.find(Employee.class,1);
+
+                        if(ee2 != null ) {
+                            ee2.setAddress("Cite International");
+                            System.out.println(ee.toString());
+                        }
+            */
+
+        }
+        finally {
+            em.close();
+        }
+
+    }
+
+}
