13e14901048551d47ae0eb321e337ced08401435
Masood2000 <masudurrehman2000@gmail.com>
2026-03-19 14:04:18 +0100

    module-end: 4

--- src/main/java/com/masood/springjpahibernate/m_4/HibernateClass.java
+++ src/main/java/com/masood/springjpahibernate/m_4/HibernateClass.java
@@ -12,6 +12,8 @@ import org.hibernate.jpa.HibernatePersistenceProvider;
 import java.util.HashMap;
 
 
+
+
 // em.persist() -> Adding an entity in context
 // em.find()    -> Find by PK. Get from DB and add it to the context if not already.
 // em.remove()  -> Marking entity for removal.
@@ -31,6 +33,7 @@ public class HibernateClass {
         props.put("hibernate.show_sql","true");
         props.put("hibernate.hbm2ddl.auto","create"); //(create tables automatically) //always use with examples and tutorials. none -> production, create -> drop then create, update -> update tables only.
 
+        
         //EntityManagerFactory emf = Persistence.createEntityManagerFactory("oraclePU");
         EntityManagerFactory emf = new HibernatePersistenceProvider()
                 .createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(puName,password), props);
